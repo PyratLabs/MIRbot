df702351edaadacb81d79ded991a796db03de0df
Xan Manning <xan.manning@gmail.com>
2017-01-15 14:20:53 +0000

    Possible fix to Vacuum

--- mirbot/__init__.py
+++ mirbot/__init__.py
@@ -1956,7 +1956,11 @@ class MIRCore:
             'username': self.buffer['username']
         }
 
-        self.db_exec("VACUUM;")
+        if sys.version_info > (3,5):
+            self.db_exec("END TRANSACTION; VACUUM;")
+        else:
+            self.db_exec("VACUUM;")
+
         self.log(aBuffer)
         self.notice("Database vacuumed.")
 
