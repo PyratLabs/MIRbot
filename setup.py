3bff53697d708e1a8e6d924043aba79a621ad87a
Xan Manning <xan.manning@gmail.com>
2019-11-20 20:38:57 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 from setuptools import setup
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+with open('README.md') as f:
+  long_description = f.read()
 
-setup(name='mirbot',
-      version='3.0.5',
-      description='Modular Information Retrieval Bot (MIRbot)',
-      long_description=readme(),
-      url='https://github.com/PyratLabs/MIRbot',
-      author='PyratLabs',
-      author_email='xan@manning.io',
-      license='MIT',
-      packages=['mirbot'],
-      scripts=['bin/mirbot'],
-      install_requires=['python-daemon'],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      classifiers = [
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-      ],
-      zip_safe=False)
+setup(
+  name='mirbot',
+  version='3.0.5',
+  description='Modular Information Retrieval Bot (MIRbot)',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/PyratLabs/MIRbot',
+  author='PyratLabs',
+  author_email='xan@manning.io',
+  license='MIT',
+  packages=['mirbot'],
+  scripts=['bin/mirbot'],
+  install_requires=['python-daemon'],
+  test_suite='nose.collector',
+  tests_require=['nose'],
+  classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+  ],
+  zip_safe=False
+)
